7c29a4d86c9cc01bf2dd6fc797f77a252181dec8
fsantos93x@gmail.com <fsantos93x@gmail.com>
2024-03-25 02:11:40 -0300

    update project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,6 +13,13 @@ const Header = () => {
   const pathname = useLocation();
   const [openNavigation, setOpenNavigation] = useState(false);
 
+  const handleClick = () => {
+      if(!openNavigation) return;
+
+      enablePageScroll();
+      setOpenNavigation(false);
+    }
+
   const toggleNavigation = () => {
       if (openNavigation) {
         setOpenNavigation(false);
@@ -21,14 +28,6 @@ const Header = () => {
         setOpenNavigation(true);
         disablePageScroll();
       }
-    };
-  
-    const handleClick = () => {
-      if (!openNavigation) return;
-  
-      enablePageScroll();
-      setOpenNavigation(false);
-    };
 
   };
 
@@ -38,11 +37,11 @@ const Header = () => {
         openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
       }`}
     >
-      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4"></div>
+      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
       <a href="#hero" className="block w-[12rem] xl:mr-8">
         <img src={brainwave} width={190} height={40} alt="Brainwave" />
       </a>
-
+      
       <nav
         className={` ${
           openNavigation ? "flex" : "hidden"
@@ -86,6 +85,7 @@ const Header = () => {
       >
         <MenuSvg openNavigation={openNavigation} />
       </Button>
+    </div>
     </div>
   );
 };
