f17439e49093f7bb56ed363454f281b307cf3900
fsantos93x@gmail.com <fsantos93x@gmail.com>
2024-03-27 05:28:04 -0300

    create hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
-
-import ButtonGradient from "./assets/svg/ButtonGradient"
-import Header from "./components/Header"
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const App = () => {
-
   return (
     <>
-      
-    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-      <Header />
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+      </div>
 
-    </div>
-    
-    <ButtonGradient />
+      <ButtonGradient />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
