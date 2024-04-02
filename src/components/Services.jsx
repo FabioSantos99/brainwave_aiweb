636796fffb3fbc1458d15b2d555ce5c85fc3963d
fsantos93x@gmail.com <fsantos93x@gmail.com>
2024-04-02 06:04:31 -0300

    roadmap pricing and footer finished

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -74,7 +74,7 @@ const Services = () => {
             <div className="py-12 px-4 xl:px-8">
                   <h4 className="h4 mb-4">Video Generation</h4>
                   <p className="body-2 mb-[2rem] text-n-3">
-                        The world's most powerful AI photo and video art generation engine. What will you create?
+                        The worlds most powerful AI photo and video art generation engine. What will you create?
                   </p>
 
                   <ul className="flex items-center justify-between">
